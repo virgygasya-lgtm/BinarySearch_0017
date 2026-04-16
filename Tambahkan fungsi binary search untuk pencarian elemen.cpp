502a5d74e68f7f906f1c9d110693d03fce6c79fb
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-04-16 15:17:51 +0700

    Tambahkan fungsi binary search untuk pencarian elemen.cpp

--- Tambahkan fungsi binary search untuk pencarian elemen.cpp
+++ Tambahkan fungsi binary search untuk pencarian elemen.cpp
@@ -36,7 +36,24 @@ void input()
         cin >> element[i];
     }
 }
+void bubblesortArray
 
+   int pass = 1;
+   do
+   {
+    
+    for int j = 0; j<+ npanjang - 1 - pass; j++
+    {
+        if element {j} > element{j + 1};
+        
+          int temp = element{j};
+          element{j} = element{j} + 1};
+          element{j + 1} = temp;
+    }
+    pass++;
+} while (pass <= npanjang - 1);
+   
+   
 
 
 
