fa4a8c48e0ab4915005fc30f5763725527b54e77
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-04-16 15:06:07 +0700

    Tambahkan fungsi input dengan validasi jumlah elemen.cpp

---
+++ Tambahkan fungsi input dengan validasi jumlah elemen.cpp
@@ -0,0 +1,29 @@
+#include  <iostream>
+using namespace std;
+
+int element[10];
+int npanjang;
+int x;
+
+void input ()
+{
+    while (true)
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array maksimal 10: ";
+        cin >> panjang;
+
+        if (panjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n!! Jumlah elemen tidak boleh lebih dari 10. Silakan coba lagi.\n";
+        }
+    }
+
+
