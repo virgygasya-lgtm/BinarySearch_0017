a854679a8951f480c6c963241fcc141e3441a183
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-04-16 15:48:46 +0700

    Tambahkan opsi pencarian berulang dan tampilan array terurut.cpp

--- Tambahkan opsi pencarian berulang dan tampilan array terurut.cpp
+++ Tambahkan opsi pencarian berulang dan tampilan array terurut.cpp
@@ -80,44 +80,4 @@ void binarySearch()
         cout << "Masukkan elemen yang ingin dicari: ";
         cin >> x;
 
-        int low = 0;
-        int high = nPanjang - 1;
-
-        do
-        {
-            int mid = (low + high) / 2;
-
-            if (element[mid] == x)
-            {
-                cout << "\n[✓] Elemen " << x << " ditemukan pada indeks " << mid << "\n";
-                break;
-            }
-            else if (x < element[mid])
-            {
-                high = mid - 1;
-            }
-            else
-            {
-                low = mid + 1;
-            }
-
-        } while (low <= high);
-
-        if (low > high)
-        {
-            cout << "\n[x] Elemen " << x << " tidak ditemukan dalam array.\n";
-        }
-
-        cout << "\nIngin mencari lagi? (y/n): ";
-        cin >> ulang;
-
-    } while (ulang == 'y' || ulang == 'Y');
-}
-
-int main()
-{
-    input();
-    bubbleSortArray();
-    display();
-    binarySearch();
-}
+       
